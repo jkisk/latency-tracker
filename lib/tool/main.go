e58aa928d84e2a2acc35c034a5e93997122d270e
Jamie Kinkead <jkinkead@gmail.com>
2019-12-22 15:48:20 -0800

    change approach

--- lib/tool/main.go
+++ lib/tool/main.go
@@ -2,54 +2,96 @@ package main
 
 import (
 	"fmt"
-	"github.com/jkisk/latency-tracker/lib/datafile"
 	"io/ioutil"
 	"log"
-	"sort"
+
+	"github.com/jkisk/latency-tracker/lib/datafile"
 )
 
-// Buckets keeps a slice of results at the 50th, 95th, and 99th percentile
+//Buckets keeps a slice of results at the 50th, 95th, and 99th percentile
 //for each batch of 10k ints representing latency times, and a count of batches.
-// type buckets struct {
-// 	P50, P95, P99 []int
-// 	BatchCount    int
+type buckets struct {
+	Mapsy       map[int]int
+	SampleCount int
+}
+
+// func (b *buckets) reportPercentile(p int) int {
+// 	for _, item := range Mapsy
 // }
 
-// func (b *buckets) fillBuckets() int {
-// }
+func (b *buckets) makeMapsyBuckets() {
+	b.Mapsy = make(map[int]int)
+	b.Mapsy[500] = 0
+	b.Mapsy[1000] = 0
+	b.Mapsy[1500] = 0
+	b.Mapsy[2000] = 0
+}
 
-// reportOut sums the entries in the bucket slices, then divides by the total
-// to track running percentiles to report out as the program runs.
-// func (b *buckets) reportOut() int {
-// 	total := 0
-// 	for _, v := range b {
-// 		total += v
-// 	}
-// 	return total / b.BatchCount
-//}
+// reports out which bucket a given percentile would fall in
+// func (b *buckets) reportPercentile(int) int {
+
+// }
 
 func main() {
+	b := new(buckets)
+	b.makeMapsyBuckets()
+	b.SampleCount = 0
+
 	files, err := ioutil.ReadDir("test-data")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, file := range files {
-		chunk, err := datafile.GetInts("test-data/" + cdfile.Name())
+		chunk, err := datafile.GetInts("test-data/" + file.Name())
 		if err != nil {
 			log.Fatal(err)
 		}
-		sortAndRecord(chunk)
+		sortAndRecord(chunk, b)
 	}
 	return
 }
 
-func sortAndRecord(chunk []int) {
-	sort.Ints(chunk)
-	//console log p50 p95 p99
-	fmt.Printf("CURRENT BATCH: p50: %v, p95: %v, p99: %v", chunk[5000], chunk[9500], chunk[9900])
-	//update batchCount, add p values to slices, console log updated average of slices
-	// fmt.Printf("TOTAL RUN: p50: %v, p95: %v, p99: %v", b.P50.reportOut(), b.P95.reportOut(), b.P99.reportOut())
+func sortAndRecord(chunk []int, b *buckets) {
+	// move all below to method?
+	// sort.Ints(chunk)
+
+	// P50 := chunk[5000]
+	// P95 := chunk[9500]
+	// P99 := chunk[9900]
+	// //console log p50 p95 p99
+	// fmt.Printf("CURRENT BATCH:\n P50: %v\n P95: %v\n P99: %v\n", P50, P95, P99)
+
+	// prepare counts for buckets
+	one := 0
+	two := 0
+	three := 0
+	four := 0
+
+	for _, item := range chunk {
+		if item <= 500 {
+			one += 1
+		} else if item <= 1000 {
+			two += 1
+		} else if item <= 1500 {
+			three += 1
+		} else {
+			four += 1
+		}
+	}
+
+	//update buckets
+	b.Mapsy[500] += one
+	b.Mapsy[1000] += two
+	b.Mapsy[1500] += three
+	b.Mapsy[2000] += four
+
+	//update batchCount
+	b.SampleCount += len(chunk)
+	//, console log updated average of slices
+	fmt.Println(b.SampleCount)
+	fmt.Println(b.Mapsy[2000])
+	// fmt.Printf("TOTAL RUN:\n P99: %v\n", b.report99())
 }
 
 //compute percentiles and update and report them every 10k items
--- lib/tool/test-main.go
+++
@@ -1,9 +0,0 @@
-package test
-
-import (
-	"testing"
-)
-
-func TestTool(t *testing.T) {
-	t.Run()
-}
